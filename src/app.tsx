d5692241993da47c1a482331f7f4dbe469b5d392
bally <bally.lomibao@howickltd.com>
2024-03-04 08:33:26 +1300

    🐞 fix(all components on App page):

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,29 @@
+import { Fragment } from 'react'
 import { Home } from 'page'
+import { AppNavBar, AppFooter } from 'component'
+import {
+  ProductCategory,
+  ProductHero,
+  ProductValue,
+  ProductHowItWorks,
+  ProductSmokingHero,
+  ProductCTA,
+} from 'page/product'
+import withRoot from 'withroot'
 
 function App() {
-  return <Home />
+  return (
+    <Fragment>
+      <AppNavBar />
+      <ProductHero />
+      <ProductValue />
+      <ProductCategory />
+      <ProductHowItWorks />
+      <ProductCTA />
+      <ProductSmokingHero />
+      <AppFooter />
+    </Fragment>
+  )
 }
 
-export default App
+export default withRoot(App)

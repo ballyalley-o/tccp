81cc497c85135c2f435105ec261c83a1539efd07
bally <bally.lomibao@howickltd.com>
2024-03-05 08:07:12 +1300

    Add Logo component with forwardRef

---
+++ src/component/logo/logo.tsx
@@ -0,0 +1,52 @@
+import { forwardRef } from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import { Box, Link } from '@mui/material'
+
+interface ILogoProps {
+  width?: number
+  disabledLink?: boolean
+  sx?: any
+  src?: string
+  [key: string]: any
+}
+const Logo = forwardRef(
+  (
+    {
+      width = 40,
+      disabledLink = false,
+      sx,
+      src = '/asset/logo.png',
+      ...other
+    }: ILogoProps,
+    ref
+  ) => {
+    // Uncomment the lines below when you're ready to use these variables.
+    // const theme = useTheme();
+    // const PRIMARY_LIGHT = theme.palette.primary.light;
+    // const PRIMARY_MAIN = theme.palette.primary.main;
+    // const PRIMARY_DARK = theme.palette.primary.dark;
+
+    // OR using local (public folder)
+    // -------------------------------------------------------
+    const logo = (
+      <Box
+        component='img'
+        src={src}
+        {...other}
+        sx={{ width, height: 'auto', cursor: 'pointer', ...sx }}
+      />
+    )
+
+    if (disabledLink) {
+      return logo
+    }
+
+    return (
+      <Link component={RouterLink} to='/' sx={{ display: 'contents' }}>
+        {logo}
+      </Link>
+    )
+  }
+)
+
+export default Logo

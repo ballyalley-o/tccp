3ea0a61727ef667d3adcbb889faba5a93874c25f
bally <bally.lomibao@howickltd.com>
2024-03-06 00:38:27 +1300

    Commented out toolbar styles

--- src/component/toolbar/toolbar.tsx
+++ src/component/toolbar/toolbar.tsx
@@ -1,11 +1,18 @@
-import { styled } from '@mui/material/styles'
-import MuiToolbar from '@mui/material/Toolbar'
+// import { styled } from '@mui/material/styles'
+// import MuiToolbar from '@mui/material/Toolbar'
 
-const Toolbar = styled(MuiToolbar)(({ theme }) => ({
-  height: 64,
-  [theme.breakpoints.up('sm')]: {
-    height: 70,
-  },
-}))
+// const Toolbar = styled(MuiToolbar)(({ theme }) => ({
+//   justifyContent: 'space-between',
+//   minHeight: 50,
+//   padding: theme.spacing(0, 1),
+//   [theme.breakpoints.up('sm')]: {
+//     minHeight: 50,
+//     padding: theme.spacing(0, 1),
+//   },
+//   [theme.breakpoints.up('md')]: {
+//     minHeight: 50,
+//     padding: theme.spacing(0, 5),
+//   },
+// }))
 
-export default Toolbar
+// export default Toolbar

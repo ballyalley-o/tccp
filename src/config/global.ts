c1604e75dcca99a783ecd3cccd17b5896c16998a
bally <bally.lomibao@howickltd.com>
2024-03-06 00:38:34 +1300

    Add global configuration file

---
+++ src/config/global.ts
@@ -0,0 +1,10 @@
+const GLOBAL = {
+  APP_NAME: 'The Code Coach Projct',
+  APP_VERSION: '1.0.3',
+  APP_DESCRIPTION:
+    'The Code Coach is a platform that provides a wide range of coaching services to help you improve your coding skills.',
+  APP_KEYWORDS: 'code, coach, coding, skills, platform, coaching, improve',
+  APP_AUTHOR: 'The Code Coach Team',
+}
+
+export default GLOBAL

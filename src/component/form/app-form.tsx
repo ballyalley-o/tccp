eb2a7f83a10ee08316493a96b7f9973a81fbfb6b
bally <bally.lomibao@howickltd.com>
2024-03-12 07:20:14 +1300

    Refactor form component styling

--- src/component/form/app-form.tsx
+++ src/component/form/app-form.tsx
@@ -1,29 +1,38 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Paper from '../components/Paper';
+import * as React from 'react'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import { Paper } from 'component/paper'
+import { ASSET } from 'config'
 
 export default function AppForm(props: React.HTMLAttributes<HTMLDivElement>) {
-  const { children } = props;
+  const { children } = props
 
   return (
     <Box
       sx={{
         display: 'flex',
-        backgroundImage: 'url(/static/onepirate/appCurvyLines.png)',
+        backgroundImage: `url(${ASSET.LINE_BG})`,
         backgroundRepeat: 'no-repeat',
+        margin: 'auto',
+        fontSize: '1.2rem',
+        color: 'common.white',
       }}
     >
-      <Container maxWidth="sm">
-        <Box sx={{ mt: 7, mb: 12 }}>
+      <Container maxWidth='sm'>
+        <Box sx={{ mt: 20, mb: 12, height: '100vh', alignContent: 'center' }}>
           <Paper
-            background="light"
-            sx={{ py: { xs: 4, md: 8 }, px: { xs: 3, md: 6 } }}
+            background='dark'
+            sx={{
+              py: { xs: 4, md: 8 },
+              px: { xs: 3, md: 6 },
+              fontSize: '1.2rem',
+              color: 'common.white',
+            }}
           >
             {children}
           </Paper>
         </Box>
       </Container>
     </Box>
-  );
+  )
 }

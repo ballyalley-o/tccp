18f4e0864333ec2726998957561db911eaa3b5a9
bally <bally.lomibao@howickltd.com>
2024-03-12 07:38:12 +1300

    Update react-router-dom import and use BrowserRouter instead of Router

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
 import { Fragment, useEffect } from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import { AppNavBar, AppFooter } from 'component'
+import Router from 'route'
 import { Dashboard } from 'page/dashboard'
 import withRoot from 'withroot'
 import GLOBAL from 'config/global'
@@ -14,9 +15,10 @@ function App() {
   return (
     <Fragment>
       <HelmetProvider>
-        <Router>
+        <BrowserRouter>
+          <Router />
           <Dashboard />
-        </Router>
+        </BrowserRouter>
       </HelmetProvider>
     </Fragment>
   )

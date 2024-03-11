fc03cdbe1621806968ddc9bc002559b6e78e4f30
bally <bally.lomibao@howickltd.com>
2024-03-12 07:37:52 +1300

    implemented global configurations for dimensions

--- src/theme/style/main.ts
+++ src/theme/style/main.ts
@@ -1,17 +1,18 @@
 import MuiToolbar from '@mui/material/Toolbar'
 import { styled } from '@mui/material/styles'
-import { Drawer, ListItem } from '@mui/material'
+import { Drawer, ListItem, Box } from '@mui/material'
+import { APP_NAVBAR } from 'config'
 
 export const SToolbar = styled(MuiToolbar)(({ theme }) => ({
   justifyContent: 'space-between',
-  minHeight: 50,
+  minHeight: APP_NAVBAR.HEIGHT,
   padding: theme.spacing(0, 1),
   [theme.breakpoints.up('sm')]: {
-    minHeight: 50,
+    minHeight: APP_NAVBAR.HEIGHT,
     padding: theme.spacing(0, 1),
   },
   [theme.breakpoints.up('md')]: {
-    minHeight: 50,
+    minHeight: APP_NAVBAR.HEIGHT,
     padding: theme.spacing(0, 5),
   },
 }))
@@ -37,4 +38,22 @@ export const SListItem = styled(ListItem)(({ theme }) => ({
     animation: '0.5s',
     ease: 'ease-in-out',
   },
+}))
+
+export const SBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  width: '12%',
+  height: 60,
+  overflow: 'hidden',
+  position: 'absolute',
+  top: 0,
+  right: -20,
+  zIndex: -1,
+  backgroundColor: theme.palette.secondary.main,
+  backgroundPosition: 'right',
+  backgroundSize: 'cover',
+  transform: 'scale(1.0) skew(30deg)',
+  transition: 'all 0.5s ease',
 }))

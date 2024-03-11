2b085d3b5369b9abc27b61295cb90a79192f008b
bally <bally.lomibao@howickltd.com>
2024-03-12 07:34:14 +1300

    Add new constants for response and label

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,2 +1,5 @@
 export { default as SOCIAL } from './social'
 export { default as LANG } from './lang'
+export { default as RESPONSE } from './response'
+export { default as LABEL } from './label'
+export { BUTTON } from './button'

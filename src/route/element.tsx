88fff59e9f4dfb2e98188b9e2d87f660186b8c17
bally <bally.lomibao@howickltd.com>
2024-03-13 07:44:12 +1300

    Add NotFoundPage component to route/element.tsx

--- src/route/element.tsx
+++ src/route/element.tsx
@@ -16,3 +16,5 @@ const Loadable = (
 export const Home = Loadable(() => import('page/home/home'))
 export const Dashboard = Loadable(() => import('page/dashboard/dashboard'))
 export const LogIn = Loadable(() => import('page/auth/log-in/log-in'))
+// fallback
+export const NotFoundPage = Loadable(() => import('page/fallback/not-found'))

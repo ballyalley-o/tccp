93ac816511a368d1ce6ee160be2dabe8cfbeb369
bally <bally.lomibao@howickltd.com>
2024-03-13 22:03:43 +1300

    Update fallback pages in route/index.tsx

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,7 +1,9 @@
+import { CODE } from 'constant'
 import { Navigate, useRoutes } from 'react-router-dom'
-import { Home, Dashboard, LogIn, NotFoundPage } from 'route/element'
-import { PATH } from 'route/param'
+import { Home, Dashboard, LogIn, FallbackPage } from 'route/element'
 import { FallbackPath } from 'route/path'
+import { PATH } from 'route/param'
+import { FALLBACK } from 'constant'
 
 function Router() {
   return useRoutes([
@@ -27,10 +29,60 @@ function Router() {
         },
       ],
     },
+    // @fallback
     {
-      // @fallback
+      path: FallbackPath.NOT_AUTHORIZED,
+      element: (
+        <FallbackPage
+          errorCode={CODE.NOT_AUTHORIZED}
+          fallbackTitle={FALLBACK.NOT_AUTHORIZED.MESSAGE}
+        />
+      ),
+    },
+    {
+      path: FallbackPath.FORBIDDEN,
+      element: (
+        <FallbackPage
+          errorCode={CODE.FORBIDDEN}
+          fallbackTitle={FALLBACK.FORBIDDEN.MESSAGE}
+        />
+      ),
+    },
+    {
       path: FallbackPath.NOT_FOUND,
-      element: <NotFoundPage />,
+      element: (
+        <FallbackPage
+          errorCode={CODE.NOT_FOUND}
+          fallbackTitle={FALLBACK.NOT_FOUND.MESSAGE}
+        />
+      ),
+    },
+    {
+      path: FallbackPath.BAD_REQUEST,
+      element: (
+        <FallbackPage
+          errorCode={CODE.BAD_REQUEST}
+          fallbackTitle={FALLBACK.BAD_REQUEST.MESSAGE}
+        />
+      ),
+    },
+    {
+      path: FallbackPath.UNPROCESSABLE_ENTITY,
+      element: (
+        <FallbackPage
+          errorCode={CODE.UNPROCESSABLE_ENTITY}
+          fallbackTitle={FALLBACK.UNPROCESSABLE_ENTITY.MESSAGE}
+        />
+      ),
+    },
+    {
+      path: FallbackPath.SERVER_ERROR,
+      element: (
+        <FallbackPage
+          errorCode={CODE.INTERNAL_SERVER_ERROR}
+          fallbackTitle={FALLBACK.SERVER_ERROR.MESSAGE}
+        />
+      ),
     },
     { path: '*', element: <Navigate to={FallbackPath.NOT_FOUND} replace /> },
   ])

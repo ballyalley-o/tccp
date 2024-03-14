5441fd2f30be402e8c550edfa6e33baa4dea94b2
bally <bally.lomibao@howickltd.com>
2024-03-15 07:37:24 +1300

    Refactor AuthForgotPassword component to include additional label and link

--- src/section/auth/auth-forgot-password.tsx
+++ src/section/auth/auth-forgot-password.tsx
@@ -1,26 +1,55 @@
+import { FC, Fragment } from 'react'
 import { Link, Typography } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import { AuthPath } from 'route/path'
 import { LABEL } from 'constant'
 
-const AuthForgotPassword = () => {
+interface AuthForgotPasswordProps {
+  label: string
+  labelSub: string
+  href: string
+}
+
+const AuthForgotPassword = ({
+  label,
+  labelSub,
+  href,
+}: AuthForgotPasswordProps) => {
   const theme = useTheme()
   return (
-    <Typography variant='subtitle2' align='center'>
-      <Link
-        underline='always'
-        href={AuthPath.FORGOT_PASSWORD}
-        sx={{
-          color: theme.palette.grey[500],
-          '&:hover': {
-            color: theme.palette.common.white,
-            textDecoration: 'none',
-          },
-        }}
-      >
-        {LABEL.FORGOT_PASSWORD}
-      </Link>
-    </Typography>
+    <Fragment>
+      <Typography variant='subtitle2' align='center' color='grey.500'>
+        {label}
+        <Link
+          href={href}
+          align='center'
+          sx={{
+            color: theme.palette.secondary.main,
+            '&:hover': {
+              color: theme.palette.common.white,
+              textDecoration: 'none',
+            },
+          }}
+        >
+          {labelSub}
+        </Link>
+      </Typography>
+      <Typography variant='subtitle2' align='center'>
+        <Link
+          underline='always'
+          href={AuthPath.FORGOT_PASSWORD}
+          sx={{
+            color: theme.palette.grey[500],
+            '&:hover': {
+              color: theme.palette.common.white,
+              textDecoration: 'none',
+            },
+          }}
+        >
+          {LABEL.FORGOT_PASSWORD}
+        </Link>
+      </Typography>
+    </Fragment>
   )
 }
 

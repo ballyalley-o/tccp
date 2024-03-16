a44d9630697f3d6b3f58e3462f8b898d26e4a905
bally <bally.lomibao@howickltd.com>
2024-03-17 03:00:55 +1300

    TCCPISSUE49: version-2: upload field component and add avatar editor

--- src/component/uploadfield/uploadfield.tsx
+++ src/component/uploadfield/uploadfield.tsx
@@ -1,39 +1,45 @@
 import { useState, ChangeEventHandler, SetStateAction } from 'react'
-import { m } from 'framer-motion'
-import {
-  Button,
-  Box,
-  Typography,
-  Input,
-  Grid,
-  CardMedia,
-  IconButton,
-} from '@mui/material'
+import AvatarEditor from 'react-avatar-editor'
+import { Button, Grid, IconButton, Dialog, DialogContent } from '@mui/material'
 import { BUTTON } from 'constant'
 import { ICON_WEB_NAME } from 'config'
 import { useIcon } from 'hook'
 
 const UploadField = () => {
-  const [file, setFile] = useState<null | File>(null)
-  const [previewUrl, setPreviewUrl] = useState<null | string>(null)
-  const [hovered, setHovered] = useState<boolean>(false)
+  const [file, setFile] = useState<File | null>(null)
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null)
+  const [openDialog, setOpenDialog] = useState(false)
+  const [hovered, setHovered] = useState(false)
+  const [editor, setEditor] = useState<AvatarEditor | null>(null)
+  const [uploaded, setUploaded] = useState(false)
 
   const { Icon, iconSrc: clearSrc } = useIcon(ICON_WEB_NAME.CLOSE)
   const { iconSrc: uploadSrc } = useIcon(ICON_WEB_NAME.UPLOAD)
 
-  const handleFileChange: React.ChangeEventHandler<HTMLInputElement> = (
-    event
-  ) => {
+  const handleFileChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     const selectedFile = event.target.files?.[0]
     if (selectedFile) {
       setFile(selectedFile)
       setPreviewUrl(URL.createObjectURL(selectedFile))
+      setOpenDialog(true)
     }
   }
 
   const handleClearPreview = () => {
     setFile(null)
     setPreviewUrl(null)
+    setOpenDialog(false)
+  }
+
+  const onSave = () => {
+    if (editor) {
+      const canvas = editor.getImageScaledToCanvas()
+      const previewUrl = canvas.toDataURL()
+      setPreviewUrl(previewUrl)
+    }
+    console.log('Image saved')
+    setUploaded(true)
+    setOpenDialog(false)
   }
 
   return (
@@ -54,22 +60,25 @@ const UploadField = () => {
             component='span'
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
+            disabled={uploaded}
             sx={{
               backgroundColor: 'common.white',
               color: 'primary.main',
               height: 60,
+              width: '100%',
               '&:hover': {
                 backgroundColor: 'secondary.main',
               },
+              '&.Mui-disabled': {
+                backgroundColor: 'common.white',
+                color: 'grey.500',
+              },
             }}
           >
-            {hovered ? (
-              <Icon
-                icon={uploadSrc}
-                sx={{
-                  height: 40,
-                }}
-              />
+            {uploaded ? (
+              BUTTON.UPLOADED_AVATAR
+            ) : hovered ? (
+              <Icon icon={uploadSrc} />
             ) : (
               BUTTON.UPLOAD_AVATAR
             )}
@@ -77,9 +86,63 @@ const UploadField = () => {
         </label>
       </Grid>
       <Grid item lg={6}>
+        <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+          <DialogContent>
+            <AvatarEditor
+              ref={(editor: AvatarEditor | null) => setEditor(editor)}
+              image={previewUrl || 'default_image_path'}
+              width={250}
+              height={250}
+              border={50}
+              borderRadius={125} // This will make the avatar circle
+              color={[255, 255, 255, 0.6]}
+              scale={1}
+              rotate={0}
+            />
+            <IconButton
+              onClick={handleClearPreview}
+              sx={{
+                position: 'absolute',
+                right: 0,
+                top: 0,
+                color: 'common.black',
+
+                opacity: 0.8,
+                zIndex: 1,
+                cursor: 'pointer',
+                margin: 'auto',
+                '&:hover': {
+                  backgroundColor: 'common.white',
+                  opacity: 0.6,
+                },
+              }}
+            >
+              <Icon icon={clearSrc} />
+            </IconButton>
+            <Button onClick={onSave}>Save</Button>
+          </DialogContent>
+        </Dialog>
         {previewUrl && (
-          <div style={{ position: 'relative', display: 'inline-block' }}>
-            <img src={previewUrl} alt='preview' style={{ height: 80 }} />
+          <div
+            style={{
+              position: 'relative',
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            {uploaded && (
+              <img
+                src={previewUrl}
+                alt='preview'
+                style={{
+                  maxHeight: 150,
+                  borderRadius: '50%',
+                  objectFit: 'cover',
+                  overflow: 'hidden',
+                }}
+              />
+            )}
+
             {previewUrl && (
               <IconButton
                 onClick={handleClearPreview}

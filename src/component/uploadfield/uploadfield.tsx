3b2a1fa1bef7e4926efbdfe5f18af598fc2a6e8f
bally <bally.lomibao@howickltd.com>
2024-03-17 01:32:50 +1300

    version-1: Add file preview and clear functionality to UploadField component

--- src/component/uploadfield/uploadfield.tsx
+++ src/component/uploadfield/uploadfield.tsx
@@ -1,10 +1,25 @@
 import { useState, ChangeEventHandler, SetStateAction } from 'react'
-import { Button, Box, Typography, Input, Grid } from '@mui/material'
+import { m } from 'framer-motion'
+import {
+  Button,
+  Box,
+  Typography,
+  Input,
+  Grid,
+  CardMedia,
+  IconButton,
+} from '@mui/material'
 import { BUTTON } from 'constant'
+import { ICON_WEB_NAME } from 'config'
+import { useIcon } from 'hook'
 
 const UploadField = () => {
   const [file, setFile] = useState<null | File>(null)
   const [previewUrl, setPreviewUrl] = useState<null | string>(null)
+  const [hovered, setHovered] = useState<boolean>(false)
+
+  const { Icon, iconSrc: clearSrc } = useIcon(ICON_WEB_NAME.CLOSE)
+  const { iconSrc: uploadSrc } = useIcon(ICON_WEB_NAME.UPLOAD)
 
   const handleFileChange: React.ChangeEventHandler<HTMLInputElement> = (
     event
@@ -22,7 +37,7 @@ const UploadField = () => {
   }
 
   return (
-    <Grid container my={4} spacing={2}>
+    <Grid container my={4} spacing={2} flexDirection='row'>
       <Grid item lg={6}>
         <input
           type='file'
@@ -37,23 +52,57 @@ const UploadField = () => {
             fullWidth
             variant='contained'
             component='span'
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
             sx={{
-              backgroundColor: 'secondary.main',
+              backgroundColor: 'common.white',
+              color: 'primary.main',
+              height: 60,
+              '&:hover': {
+                backgroundColor: 'secondary.main',
+              },
             }}
           >
-            {BUTTON.UPLOAD_AVATAR}
+            {hovered ? (
+              <Icon
+                icon={uploadSrc}
+                sx={{
+                  height: 40,
+                }}
+              />
+            ) : (
+              BUTTON.UPLOAD_AVATAR
+            )}
           </Button>
         </label>
       </Grid>
       <Grid item lg={6}>
         {previewUrl && (
-          <Box>
-            {/* <Typography variant='subtitle1'>Preview:</Typography> */}
-            <img src={previewUrl} alt='Preview' style={{ height: 80 }} />
-            <Button onClick={handleClearPreview} sx={{ color: '#FFF' }}>
-              {BUTTON.CLEAR_PREVIEW}
-            </Button>
-          </Box>
+          <div style={{ position: 'relative', display: 'inline-block' }}>
+            <img src={previewUrl} alt='preview' style={{ height: 80 }} />
+            {previewUrl && (
+              <IconButton
+                onClick={handleClearPreview}
+                sx={{
+                  position: 'absolute',
+                  right: 0,
+                  top: 0,
+                  color: 'common.black',
+                  opacity: 0.8,
+                  zIndex: 1,
+                  cursor: 'pointer',
+                  margin: 'auto',
+                  minHeight: 0,
+                  '&:hover': {
+                    backgroundColor: 'common.white',
+                    opacity: 0.6,
+                  },
+                }}
+              >
+                <Icon icon={clearSrc} />
+              </IconButton>
+            )}
+          </div>
         )}
       </Grid>
     </Grid>

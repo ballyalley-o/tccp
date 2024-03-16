50dcf6372d0ddbe0ebf90f2bbaa2682224f92fe3
bally <bally.lomibao@howickltd.com>
2024-03-16 23:38:13 +1300

    TCCPISSUE40: Add motion effect to AuthBranding component

--- src/section/auth/auth-branding.tsx
+++ src/section/auth/auth-branding.tsx
@@ -3,23 +3,26 @@ import { Grid, Typography } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import { Logo } from 'component/logo'
 import { ASSET, GLOBAL } from 'config'
+import { MotionContainer, MotionText, varFade } from 'component/motion'
 
 const AuthBranding = () => {
   const theme = useTheme()
   return (
-    <m.div>
+    <MotionContainer>
       <Grid container alignItems='center' flexDirection='column' flex={1}>
         <Logo width={80} src={ASSET.BRAND_ALT} />
-        <Typography
-          variant='h5'
-          gutterBottom
-          align='center'
-          color={theme.palette.common.white}
-        >
-          {GLOBAL.APP_NAME}
-        </Typography>
+        <MotionText
+          text={GLOBAL.APP_NAME}
+          sx={{ typography: 'h5' }}
+          variants={
+            varFade({
+              durationIn: 0.8,
+              delay: 0.5,
+            }).inUp
+          }
+        />
       </Grid>
-    </m.div>
+    </MotionContainer>
   )
 }
 

2519ee0d54e78a43f249388543b8fd7631ef9f87
bally <bally.lomibao@howickltd.com>
2024-03-17 20:20:35 +1300

    Update form component imports

--- src/component/form/index.ts
+++ src/component/form/index.ts
@@ -4,3 +4,14 @@ export { default as FormFeedback } from './form-feedback'
 export { default as RFTextField } from './rf-text-field'
 export { default as AppForm } from './app-form'
 export { email, required } from './validation'
+
+export { default as UploadField } from './upload-field/upload-field'
+export { default as AvatarEditorDialog } from './upload-field/avatar-editor-dialog'
+export * from './upload-field/style'
+
+export { default as TextField } from './text-field/text-field'
+
+export { default as PasswordField } from './form-field/password-field'
+export { default as RoleField } from './form-field/role-field'
+export { default as GithubField } from './form-field/github-field'
+export { default as FormButtonRedir } from './form-field/form-button-redir'

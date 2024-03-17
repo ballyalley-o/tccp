e2e1ca93834a3f76f2093734d22c25bd602281d5
bally <bally.lomibao@howickltd.com>
2024-03-17 17:25:08 +1300

    Add KEY enum file

---
+++ src/constant/key.ts
@@ -0,0 +1,21 @@
+enum KEY {
+  // @form
+  FIRST_NAME = 'firstName',
+  LAST_NAME = 'lastName',
+  EMAIL = 'email',
+  PASSWORD = 'password',
+  CONFIRM_PASSWORD = 'confirmPassword',
+  GITHUB_USERNAME = 'githubUsername',
+  LOCATION = 'location',
+  ROLE = 'role',
+  AVATAR = 'avatar',
+  STUDENT = 'student',
+  TRAINER = 'trainer',
+  ADMIN = 'admin',
+  //important keys
+  TEXT = 'text',
+  AUTO = 'auto',
+  END = 'end',
+}
+
+export default KEY

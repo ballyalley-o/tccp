bd53a0574b90b30b88cd6e9c6f6aff383e1c1876
bally <bally.lomibao@howickltd.com>
2024-03-18 01:25:43 +1300

    TCCPISSUE60: updated route imports and add BootcampPage

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,10 +1,12 @@
 import { CODE } from 'constant'
 import { Navigate, useRoutes } from 'react-router-dom'
 import {
-  Home,
-  Dashboard,
-  LogIn,
+  HomePage,
+  DashboardPage,
+  LogInPage,
   RegisterPage,
+  // bootcamp
+  BootcampPage,
   FallbackPage,
 } from 'route/element'
 import { FallbackPath } from 'route/path'
@@ -15,7 +17,7 @@ function Router() {
   return useRoutes([
     {
       path: PATH.ROOT,
-      element: <Dashboard />,
+      element: <DashboardPage />,
     },
     {
       // @auth
@@ -23,7 +25,7 @@ function Router() {
       children: [
         {
           path: PATH.LOG_IN,
-          element: <LogIn />,
+          element: <LogInPage />,
         },
         {
           path: PATH.REGISTER,
@@ -35,60 +37,40 @@ function Router() {
         },
       ],
     },
+    {
+      path: PATH.BOOTCAMP,
+      element: <BootcampPage />,
+      children: [
+        {
+          path: PATH.ID,
+          // element: <BootcampDetailPage />,
+        },
+      ],
+    },
     // @fallback
     {
       path: FallbackPath.NOT_AUTHORIZED,
-      element: (
-        <FallbackPage
-          errorCode={CODE.NOT_AUTHORIZED}
-          fallbackTitle={FALLBACK.NOT_AUTHORIZED.MESSAGE}
-        />
-      ),
+      element: <FallbackPage errorCode={CODE.NOT_AUTHORIZED} fallbackTitle={FALLBACK.NOT_AUTHORIZED.MESSAGE} />,
     },
     {
       path: FallbackPath.FORBIDDEN,
-      element: (
-        <FallbackPage
-          errorCode={CODE.FORBIDDEN}
-          fallbackTitle={FALLBACK.FORBIDDEN.MESSAGE}
-        />
-      ),
+      element: <FallbackPage errorCode={CODE.FORBIDDEN} fallbackTitle={FALLBACK.FORBIDDEN.MESSAGE} />,
     },
     {
       path: FallbackPath.NOT_FOUND,
-      element: (
-        <FallbackPage
-          errorCode={CODE.NOT_FOUND}
-          fallbackTitle={FALLBACK.NOT_FOUND.MESSAGE}
-        />
-      ),
+      element: <FallbackPage errorCode={CODE.NOT_FOUND} fallbackTitle={FALLBACK.NOT_FOUND.MESSAGE} />,
     },
     {
       path: FallbackPath.BAD_REQUEST,
-      element: (
-        <FallbackPage
-          errorCode={CODE.BAD_REQUEST}
-          fallbackTitle={FALLBACK.BAD_REQUEST.MESSAGE}
-        />
-      ),
+      element: <FallbackPage errorCode={CODE.BAD_REQUEST} fallbackTitle={FALLBACK.BAD_REQUEST.MESSAGE} />,
     },
     {
       path: FallbackPath.UNPROCESSABLE_ENTITY,
-      element: (
-        <FallbackPage
-          errorCode={CODE.UNPROCESSABLE_ENTITY}
-          fallbackTitle={FALLBACK.UNPROCESSABLE_ENTITY.MESSAGE}
-        />
-      ),
+      element: <FallbackPage errorCode={CODE.UNPROCESSABLE_ENTITY} fallbackTitle={FALLBACK.UNPROCESSABLE_ENTITY.MESSAGE} />,
     },
     {
       path: FallbackPath.SERVER_ERROR,
-      element: (
-        <FallbackPage
-          errorCode={CODE.INTERNAL_SERVER_ERROR}
-          fallbackTitle={FALLBACK.SERVER_ERROR.MESSAGE}
-        />
-      ),
+      element: <FallbackPage errorCode={CODE.INTERNAL_SERVER_ERROR} fallbackTitle={FALLBACK.SERVER_ERROR.MESSAGE} />,
     },
     { path: '*', element: <Navigate to={FallbackPath.NOT_FOUND} replace /> },
   ])

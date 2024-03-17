acf9c016230c0d622736aabea7ef17d3c34e0d36
bally <bally.lomibao@howickltd.com>
2024-03-17 20:21:38 +1300

    Add color constants

---
+++ src/constant/color.ts
@@ -0,0 +1,12 @@
+export enum COLOR {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+  SUCCESS = 'success',
+  ERROR = 'error',
+  WARNING = 'warning',
+  INFO = 'info',
+  COMMON = 'common',
+  BLACK = 'black',
+  WHITE = 'white',
+  GREY = 'grey',
+}

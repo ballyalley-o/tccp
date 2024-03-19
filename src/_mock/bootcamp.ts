9c51cfb08c726d7ac72203dd9a233925d69a8763
bally <bally.lomibao@howickltd.com>
2024-03-20 07:24:39 +1300

    Add mockBootcamp to _mock directory

---
+++ src/_mock/bootcamp.ts
@@ -0,0 +1,46 @@
+const mockBootcamp = [
+  {
+    name: 'IBM Bootcamp',
+    description: 'Learn Python from scratch',
+    website: 'https://www.python.org/',
+    phone: '123-456-7890',
+    email: 'educ@ibm.edu',
+    location: {
+      address: '1234 Main St',
+      city: 'San Francisco',
+      state: 'CA',
+      zip: '94111',
+      country: 'USA',
+    },
+    duration: '12 weeks',
+    averageCost: '$10,000',
+    housing: false,
+    jobAssistance: true,
+    jobGuarantee: false,
+    imageUrl: 'https://images.pexels.com/photos/2982449/pexels-photo-2982449.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    badge: 'https://m.media-amazon.com/images/I/61fLmo3aOHL.jpg',
+  },
+  {
+    name: 'IBM Bootcamp',
+    description: 'Learn Python from scratch',
+    website: 'https://www.python.org/',
+    phone: '123-456-7890',
+    email: 'educ@ibm.edu',
+    location: {
+      address: '1234 Main St',
+      city: 'San Francisco',
+      state: 'CA',
+      zip: '94111',
+      country: 'USA',
+    },
+    duration: '12 weeks',
+    averageCost: '$10,000',
+    housing: false,
+    jobAssistance: true,
+    jobGuarantee: false,
+    imageUrl: 'https://images.pexels.com/photos/2982449/pexels-photo-2982449.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    badge: 'https://m.media-amazon.com/images/I/61fLmo3aOHL.jpg',
+  },
+]
+
+export default mockBootcamp

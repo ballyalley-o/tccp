1e59764f13254bf63db46cce8bb9e86bee6b8fff
bally <bally.lomibao@howickltd.com>
2024-03-25 20:01:45 +1300

    Redux | Bootcamp Slice #15: Add bootcamp reducer and index reducer

---
+++ src/store/reducer/index.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// import bootcampReducer from './bootcamp';
+
+const rootReducer = combineReducers({
+  //   bootcamp: bootcampReducer
+});
+
+export default rootReducer;

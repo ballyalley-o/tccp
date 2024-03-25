be627b0abfb02b3124713588334cf2a608e22ad6
bally <bally.lomibao@howickltd.com>
2024-03-25 19:57:33 +1300

    Add placeholder constant enum

---
+++ src/constant/placeholder.ts
@@ -0,0 +1,20 @@
+enum PLACEHOLDER {
+  EMAIL = 'Email',
+  PASSWORD = 'Password',
+  CONFIRM_PASSWORD = 'Confirm Password',
+  FIRST_NAME = 'First Name',
+  LAST_NAME = 'Last Name',
+  PHONE_NUMBER = 'Phone Number',
+  ADDRESS = 'Address',
+  CITY = 'City',
+  COUNTRY = 'Country',
+  ZIP_CODE = 'Zip Code',
+
+  // @bootcamp search form
+  MILES_FROM = 'Miles From',
+  ENTER_ZIPCODE = 'Enter Zipcode',
+  SELECT_RATING = 'Select a rating',
+  SELECT_BUDGET = 'Select a budget'
+}
+
+export default PLACEHOLDER

f364462057afd3b34317076dfd90c78dd56851cf
bally <bally.lomibao@howickltd.com>
2024-03-25 20:20:46 +1300

    Error Boundary provider #72: Add Redux store provider to main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import 'css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from 'store'
 import App from 'app'
 
 const root = document.getElementById('root')
 ReactDOM.createRoot(root!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )

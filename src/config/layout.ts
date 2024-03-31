cbef1776fe8ef4e8c1a52d4a57ab4645fd64b6d7
bally <bally.lomibao@howickltd.com>
2024-03-31 23:39:20 +1300

    Add layout configuration constants

---
+++ src/config/layout.ts
@@ -0,0 +1,20 @@
+export const NAV = {
+  W_BASE: 260,
+  W_DASHBOARD: 300,
+  W_DASHBOARD_MINI: 88,
+
+  H_DASHBOARD_ITEM: 48,
+  H_DASHBOARD_ITEM_SUB: 36,
+
+  H_DASHBOARD_ITEM_HORIZONTAL: 25,
+  // popover
+  SPACING: 2,
+  H_OPTION: 50,
+  H_ACCOUNT_POPOVER: 380
+}
+
+export const RADIUS = {
+  BORDER: {
+    borderRadius: 2
+  }
+}

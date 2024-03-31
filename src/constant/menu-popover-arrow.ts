f2c317ec6244dd4cd888c39d5abb671888e5c147
bally <bally.lomibao@howickltd.com>
2024-03-31 23:39:36 +1300

    Add MENU_POPOVER_ARROW constant

---
+++ src/constant/menu-popover-arrow.ts
@@ -0,0 +1,16 @@
+enum MENU_POPOVER_ARROW {
+  TOP_LEFT = 'top-left',
+  TOP_CENTER = 'top-center',
+  TOP_RIGHT = 'top-right',
+  BOTTOM_LEFT = 'bottom-left',
+  BOTTOM_CENTER = 'bottom-center',
+  BOTTOM_RIGHT = 'bottom-right',
+  LEFT_TOP = 'left-top',
+  LEFT_CENTER = 'left-center',
+  LEFT_BOTTOM = 'left-bottom',
+  RIGHT_TOP = 'right-top',
+  RIGHT_CENTER = 'right-center',
+  RIGHT_BOTTOM = 'right-bottom'
+}
+
+export default MENU_POPOVER_ARROW

9cddae0896d54a375737e4a56afadb79b2aadebe
bally <bally.lomibao@howickltd.com>
2024-03-31 23:38:39 +1300

    Update dimension configuration

--- src/config/dimension.ts
+++ src/config/dimension.ts
@@ -1,17 +1,32 @@
 export enum ICON_DIMENSION {
-  FOOTER_SOCIAL_ICON = 30,
+  FOOTER_SOCIAL_ICON = 30
 }
 
 export enum APP_NAVBAR {
-  HEIGHT = 60,
+  HEIGHT = 60
 }
 
 export const RADIUS = {
   BORDER: {
-    borderRadius: 2,
-  },
+    borderRadius: 2
+  }
 }
 
 export enum APP_FIELD {
-  HEIGHT = 90,
+  HEIGHT = 90
+}
+
+export const NAV = {
+  W_BASE: 260,
+  W_DASHBOARD: 300,
+  W_DASHBOARD_MINI: 88,
+
+  H_DASHBOARD_ITEM: 48,
+  H_DASHBOARD_ITEM_SUB: 36,
+
+  H_DASHBOARD_ITEM_HORIZONTAL: 25,
+  // popover
+  SPACING: 2,
+  H_OPTION: 50,
+  H_ACCOUNT_POPOVER: 380
 }

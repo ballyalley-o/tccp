e54082e4f3fc84335ca54403df6cf82ff616ab52
bally <bally.lomibao@howickltd.com>
2024-03-31 23:33:07 +1300

    Add navbar utility files

---
+++ src/component/navbar/util/option.tsx
@@ -0,0 +1,14 @@
+// import { PATH_SECURITY } from 'route/path'
+
+const OPTION = [
+  {
+    label: 'Profile',
+    linkTo: '/'
+  },
+  {
+    label: 'Change Password',
+    linkTo: '/'
+  }
+]
+
+export default OPTION

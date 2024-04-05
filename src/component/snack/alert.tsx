d2f0972e7f33ac1f0e0aa6a684bb9bc34e1dc341
bally <bally.lomibao@howickltd.com>
2024-04-05 23:45:42 +1300

    Refactor Snack component to include Snackbar and add timer

--- src/component/snack/alert.tsx
+++ src/component/snack/alert.tsx
@@ -1,5 +1,6 @@
-import { FC } from 'react'
-import { Alert, AlertTitle } from '@mui/material'
+import { FC, useState, useEffect } from 'react'
+import { m } from 'framer-motion'
+import { Alert, AlertTitle, Snackbar } from '@mui/material'
 import { useIcon } from 'hook'
 import { ICON_NAME } from 'hook/use-icon'
 
@@ -7,10 +8,13 @@ interface IAlert {
   title: string
   message?: string
   icon?: JSX.Element
+  time?: number
   severity: 'error' | 'warning' | 'info' | 'success'
+  condition: boolean
 }
 
-const Snack: FC<IAlert> = ({ title, message, severity, icon }) => {
+const Snack: FC<IAlert> = ({ title, message, severity, icon, time, condition }) => {
+  const [open, setOpen] = useState(false)
   const { Icon, iconSrc } = useIcon(
     severity === 'error'
       ? ICON_NAME.SNACK_ERROR
@@ -21,25 +25,46 @@ const Snack: FC<IAlert> = ({ title, message, severity, icon }) => {
       : ICON_NAME.SNACK_SUCCESS
   )
 
+  useEffect(() => {
+    let timer: NodeJS.Timeout | null = null
+    if (condition) {
+      setOpen(true)
+      timer = setTimeout(() => {
+        setOpen(false)
+      }, 5000)
+    } else {
+      setOpen(false)
+    }
+    return () => {
+      if (timer) {
+        clearTimeout(timer)
+      }
+    }
+  }, [condition, setOpen])
+
   return (
-    <Alert
-      severity={severity}
-      variant="filled"
-      role="alert"
-      icon={<Icon icon={iconSrc} />}
-      sx={{
-        width: '100%',
-        marginY: 3
-      }}>
-      <AlertTitle
-        color="common.white"
-        sx={{
-          fontSize: '1rem'
-        }}>
-        {title}
-      </AlertTitle>
-      {message && message}
-    </Alert>
+    <Snackbar open={open} autoHideDuration={time || 5000}>
+      <m.div initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+        <Alert
+          severity={severity}
+          variant='filled'
+          role='alert'
+          icon={<Icon icon={iconSrc} />}
+          sx={{
+            width: '100%',
+            marginY: 3
+          }}>
+          <AlertTitle
+            color='common.white'
+            sx={{
+              fontSize: '1rem'
+            }}>
+            {title}
+          </AlertTitle>
+          {message && message}
+        </Alert>
+      </m.div>
+    </Snackbar>
   )
 }
 

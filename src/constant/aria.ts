f8b4991532215ef218d8ac266b4e2bf88844bcf3
bally <bally.lomibao@howickltd.com>
2024-04-05 23:45:49 +1300

    Add ARIA constants

---
+++ src/constant/aria.ts
@@ -0,0 +1,9 @@
+enum ARIA {
+  AUTO_COMPLETE = 'auto complete',
+  NEW_PASSWORD = 'new-password',
+  FIRST_NAME = 'First name',
+  LAST_NAME = 'Last name',
+  TOGGLE_PASSWORD = 'toggle password visibility'
+}
+
+export default ARIA

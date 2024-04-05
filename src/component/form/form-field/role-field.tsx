a162b1d5be20ae2f7efd239a615202667ee9b775
bally <bally.lomibao@howickltd.com>
2024-04-05 22:22:19 +1300

    Refactor RoleField component to use react-hook-form and update styling

--- src/component/form/form-field/role-field.tsx
+++ src/component/form/form-field/role-field.tsx
@@ -1,22 +1,65 @@
-import { MenuItem } from '@mui/material'
-import { TextField } from 'component'
+import { useState, forwardRef, ChangeEvent } from 'react'
+import { MenuItem, TextField } from '@mui/material'
+import { Controller } from 'react-hook-form'
 import { KEY, LABEL } from 'constant'
 import { FORM } from 'section/auth'
 
 const MARGIN = 'normal'
-const VARIANT = 'standard'
+const VARIANT = 'filled'
 
-const RoleField = () => {
+const RoleField = forwardRef(({ control, submitting, sent, register }: FormFieldProps, ref) => {
+  const [selectedValue, setSelectedValue] = useState('student') // Initial selected value
+
+  const handleValueChange = (event: ChangeEvent<{ value: unknown }>) => {
+    setSelectedValue(event.target.value as string)
+  }
   return (
-    <TextField margin={MARGIN} variant={VARIANT} {...FORM.ROLE}>
-      <MenuItem value='' disabled>
-        {LABEL.SELECT_ROLE}
-      </MenuItem>
-      <MenuItem value={KEY.STUDENT}>{LABEL.STUDENT}</MenuItem>
-      <MenuItem value={KEY.TRAINER}>{LABEL.TRAINER}</MenuItem>
-      <MenuItem value={KEY.ADMIN}>{LABEL.ADMIN}</MenuItem>
-    </TextField>
+    <Controller
+      name={KEY.ROLE}
+      control={control}
+      defaultValue=''
+      {...(register && { register })}
+      render={({ field }) => (
+        <TextField
+          {...field}
+          margin={MARGIN}
+          variant={VARIANT}
+          {...FORM.ROLE}
+          size='medium'
+          disabled={submitting || sent}
+          sx={{
+            color: 'common.white',
+
+            '& .MuiFilledInput-root': {
+              color: 'common.black',
+              backgroundColor: 'common.white',
+              '& .MuiInputLabel-root': {
+                color: 'common.white'
+              },
+              '&.Mui-focused .MuiInputBase-input': {
+                color: 'common.black'
+              },
+              '&.Mui-focused': {
+                backgroundColor: 'secondary.main'
+              },
+              '&:hover': {
+                backgroundColor: 'secondary.main'
+              }
+            },
+            '&:active': {
+              color: 'common.white'
+            },
+            padding: 1,
+            marginY: 1
+          }}>
+          <MenuItem disabled>{LABEL.SELECT_ROLE}</MenuItem>
+          <MenuItem value={'student'}>{LABEL.STUDENT}</MenuItem>
+          <MenuItem value={'trainer'}>{LABEL.TRAINER}</MenuItem>
+          <MenuItem value={'admin'}>{LABEL.ADMIN}</MenuItem>
+        </TextField>
+      )}
+    />
   )
-}
+})
 
 export default RoleField

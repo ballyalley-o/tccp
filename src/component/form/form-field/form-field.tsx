a635a2055b0c8b781ea9023f58ec637800e14abc
bally <bally.lomibao@howickltd.com>
2024-04-05 23:45:31 +1300

    Refactor FormField component to include error handling

--- src/component/form/form-field/form-field.tsx
+++ src/component/form/form-field/form-field.tsx
@@ -17,6 +17,7 @@ interface ExtendedFormFieldProps extends FormFieldProps {
   isGithub?: boolean
   isConfirm?: boolean
   required?: boolean
+  errors?: any
   helperText?: string
   setValue?: (value: string) => void
 }
@@ -34,6 +35,7 @@ const FormField = forwardRef(
       autoComplete,
       helperText,
       isGithub,
+      errors,
       required = false
     }: ExtendedFormFieldProps,
     ref
@@ -52,90 +54,70 @@ const FormField = forwardRef(
     }
 
     return (
-      <Controller
-        control={control}
-        name={name}
-        {...(register && { register })}
-        render={({ field, fieldState: { error } }) => (
-          <TextField
-            {...field}
-            inputRef={ref as ForwardedRef<HTMLInputElement>}
-            autoComplete={autoComplete}
-            disabled={submitting || sent}
-            variant='filled'
-            color='primary'
-            label={label}
-            placeholder={placeholder}
-            type={showPassword ? type : isPassword ? KEY.PASSWORD : type}
-            // value={typeof field.value === 'number' && field.value === 0 ? '' : field.value}
-            error={!!error || !!Error}
-            helperText={error ? error?.message : helperText}
-            // value={value}
-            // onChange={(e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
-            {...(isPassword && {
-              InputProps: {
-                endAdornment: (
-                  <InputAdornment
-                    position='end'
-                    sx={{
-                      width: KEY.AUTO
-                    }}>
-                    <IconButton
-                      edge='start'
-                      size='small'
-                      aria-label={ARIA.TOGGLE_PASSWORD}
-                      onClick={handleToggleShowPassword}
-                      sx={{
-                        width: KEY.AUTO,
-                        color: theme.palette.text.secondary
-                      }}>
-                      <WebIcon icon={showPassword ? eyeHideSrc : eyeOffSrc} />
-                    </IconButton>
-                  </InputAdornment>
-                )
-              }
-            })}
-            {...(isGithub && {
-              InputProps: {
-                startAdornment: (
-                  <InputAdornment position={KEY.START}>
-                    <LocIcon icon={githubSrc} color={theme.palette.common.black} />
-                  </InputAdornment>
-                )
-              }
-            })}
-            required={required}
-            autoFocus
-            fullWidth
-            sx={{
-              color: 'common.white',
-
-              '& .MuiFilledInput-root': {
-                color: 'common.black',
-                backgroundColor: 'common.white',
-                '& .MuiInputLabel-root': {
-                  color: 'common.white'
-                },
-                '&.Mui-focused .MuiInputBase-input': {
-                  color: 'common.black'
-                },
-                '&.Mui-focused': {
-                  backgroundColor: 'secondary.main'
-                },
-                '&:hover': {
-                  backgroundColor: 'secondary.main'
-                }
-              },
-              '&:active': {
-                color: 'common.white'
-              },
-              padding: 1,
-              marginY: 1
-            }}
-          />
-        )}
+      <TextField
+        {...(register && register)}
+        inputRef={ref as ForwardedRef<HTMLInputElement>}
+        autoComplete={autoComplete}
         disabled={submitting || sent}
-        rules={{ required: true }}
+        variant='filled'
+        color='primary'
+        label={label}
+        placeholder={placeholder}
+        type={showPassword ? type : isPassword ? KEY.PASSWORD : type}
+        error={!!Error}
+        helperText={errors[name]?.message || helperText}
+        {...(isPassword && {
+          InputProps: {
+            endAdornment: (
+              <InputAdornment position='end' sx={{ width: KEY.AUTO }}>
+                <IconButton
+                  edge='start'
+                  size='small'
+                  aria-label={ARIA.TOGGLE_PASSWORD}
+                  onClick={handleToggleShowPassword}
+                  sx={{ width: KEY.AUTO, color: theme.palette.text.secondary }}>
+                  <WebIcon icon={showPassword ? eyeHideSrc : eyeOffSrc} />
+                </IconButton>
+              </InputAdornment>
+            )
+          }
+        })}
+        {...(isGithub && {
+          InputProps: {
+            startAdornment: (
+              <InputAdornment position={KEY.START}>
+                <LocIcon icon={githubSrc} color={theme.palette.common.black} />
+              </InputAdornment>
+            )
+          }
+        })}
+        required={required}
+        autoFocus
+        fullWidth
+        sx={{
+          color: 'common.white',
+          '& .MuiFilledInput-root': {
+            color: 'common.black',
+            backgroundColor: 'common.white',
+            '& .MuiInputLabel-root': {
+              color: 'common.white'
+            },
+            '&.Mui-focused .MuiInputBase-input': {
+              color: 'common.black'
+            },
+            '&.Mui-focused': {
+              backgroundColor: 'secondary.main'
+            },
+            '&:hover': {
+              backgroundColor: 'secondary.main'
+            }
+          },
+          '&:active': {
+            color: 'common.white'
+          },
+          padding: 1,
+          marginY: 1
+        }}
       />
     )
   }

4999e00b8b1d3abad5e424c0b71ded9be8f8ba38
bally <bally.lomibao@howickltd.com>
2024-04-08 02:07:24 +1200

    authentication provider and update dependencies

--- src/auth/auth-provider.tsx
+++ src/auth/auth-provider.tsx
@@ -1,14 +1,14 @@
 import { LOCAL_STORAGE } from 'constant'
 import React, { FC, createContext, useEffect, useState, useMemo, useCallback } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
 import { createAsyncThunk } from '@reduxjs/toolkit'
-import storage from 'redux-persist/lib/storage'
-import { setCredential } from 'store/slice/auth'
-import { useLogoutMutation } from 'store/slice/auth'
 import localStorageSpace from 'util/local-storage-space'
+import storage from 'redux-persist/lib/storage'
+import { setInitial, setCredential } from 'store/slice/auth'
+import { useLoginMutation, useLogoutMutation } from 'store/slice/auth/endpoint'
 import { isValidToken, setSession } from 'auth/utility'
 import { AuthPath } from 'route/path'
 import { RESPONSE } from 'constant'
+import { useSelector, dispatch } from 'store'
 
 export const AuthContext = createContext<{
   isAuthenticated: boolean
@@ -24,13 +24,15 @@ interface AuthProviderProps {
 interface RootState {
   auth: {
     user: any | null
+    isAuthenticated: boolean
   }
 }
+
 export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-  const { user = null } = useSelector((state: RootState) => state.auth || {})
-  // const [logoutMutation] = useLogoutMutation()
-  const dispatch = useDispatch()
+  // const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const { user, isAuthenticated } = useSelector((state: RootState) => state.auth || {})
+  const [logoutCall] = useLogoutMutation()
+  const [loginCall, { isLoading }] = useLoginMutation()
 
   const storageAvailable = useMemo(() => localStorageSpace(), [])
 
@@ -40,24 +42,66 @@ export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
     setSession(token)
   }
 
-  useEffect(() => {
-    const expirationTime = localStorage.getItem('expirationTime')
-    if (expirationTime && new Date().getTime() < parseInt(expirationTime, 10)) {
-      setIsAuthenticated(true)
-    } else {
-      setIsAuthenticated(false)
-      dispatch(setCredential({ user: null }))
-      localStorage.removeItem('expirationTime')
+  // useEffect(() => {
+  //   const expirationTime = localStorage.getItem('expirationTime')
+  //   const user = JSON.parse(localStorage.getItem(LOCAL_STORAGE.USER) || '{}')
+
+  //   if (user.user !== null) {
+  //     dispatch(setCredential({ user: null, isAuthenticated: false, token: null, isInitialized: false }))
+  //     localStorage.removeItem('expirationTime')
+  //   } else {
+  //     console.log('user', user)
+  //     setIsAuthenticated(false)
+  //     dispatch(setCredential({ user: null, isAuthenticated: false, token: null, isInitialized: false }))
+  //   }
+  // }, [dispatch])
+
+  const initialize = useCallback(async () => {
+    try {
+      const token = storageAvailable ? localStorage.getItem(LOCAL_STORAGE.TOKEN) : ''
+
+      if (token && isValidToken(token)) {
+        setSession(token)
+
+        dispatch(
+          setInitial({
+            isInitialized: true,
+            user,
+            isAuthenticated
+          })
+        )
+      } else {
+        dispatch(
+          setInitial({
+            isInitialized: false,
+            isAuthenticated: false,
+            user: null
+          })
+        )
+      }
+    } catch (error) {
+      console.error(error)
+      dispatch(
+        setInitial({
+          isInitialized: false,
+          isAuthenticated: false,
+          user: null
+        })
+      )
     }
-  }, [dispatch])
+  }, [storageAvailable, dispatch])
+
+  useEffect(() => {
+    initialize()
+  }, [initialize])
 
   // clear All persisted data and remove Items from localStorage
   const clearAllPersistedStates = createAsyncThunk('auth/clearAllPersistedStates', async (_, { dispatch }) => {
     try {
       setSession(null)
-      localStorage.removeItem(LOCAL_STORAGE.NAME)
-      localStorage.removeItem(LOCAL_STORAGE.EMAIL)
+      localStorage.removeItem(LOCAL_STORAGE.USER)
       localStorage.removeItem(LOCAL_STORAGE.TOKEN)
+      localStorage.removeItem(LOCAL_STORAGE.EXPIRATION)
       window.location.href = AuthPath.LOG_IN
       const keys = Object.keys(localStorage)
       const reduxPersistKeys = keys.filter(
@@ -70,17 +114,24 @@ export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
   })
 
   const login = useCallback(
-    (credentials: { email: string; password: string; name: string; token: string }) => {
-      dispatch(setCredential(credentials))
-      setIsAuthenticated(true)
+    async (credentials: { email: string; password: string; name?: string; token?: string }) => {
+      const { email, password } = credentials
+      const res = await loginCall({
+        email,
+        password
+      }).unwrap()
+
+      dispatch(setCredential({ isAuthenticated: true, ...(res || {}) }))
+      // setIsAuthenticated(true)
     },
     [dispatch]
   )
 
   const logout = useCallback(async () => {
     try {
+      await logoutCall({}).unwrap()
       await dispatch(clearAllPersistedStates() as any)
-      setIsAuthenticated(false)
+      await dispatch(setCredential({ isAuthenticated: false, user: null, token: null }))
     } catch (error) {
       console.error(error)
     }
@@ -89,11 +140,11 @@ export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
   const memoizedValue = useMemo(
     () => ({
       isAuthenticated,
-      user: user || null,
+      user,
       login,
       logout
     }),
-    [isAuthenticated, login, logout]
+    [isAuthenticated, user, login, logout]
   )
 
   return <AuthContext.Provider value={memoizedValue}>{children}</AuthContext.Provider>

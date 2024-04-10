8fc6e8d588203ae8f40df1bec89e14df007e234a
bally <bally.lomibao@howickltd.com>
2024-04-11 02:26:15 +1200

    Add use-local-storage and use-snack hooks, remove duplicate useLocalStorage import

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -1,3 +1,4 @@
+export { default as useLocalStorage } from './use-local-storage'
+export { default as SnackProvider } from './use-snack'
 export { useResponsive } from './use-responsive'
 export * from './use-icon'
-export { default as useLocalStorage } from './use-local-storage'

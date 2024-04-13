eafd2942fd105421890d058cad38dc8a6e741dbe
bally <bally.lomibao@howickltd.com>
2024-04-13 20:43:12 +1200

    Redux | User Slice #14: Changes in auth-guard and add AuthGuard export: Implemented on routing for user account

--- src/auth/auth-guard.tsx
+++ src/auth/auth-guard.tsx
@@ -1,13 +1,12 @@
 import { useState, Fragment } from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
-import useAuthContext from './use-auth-context'
 import { AuthPath } from 'route/path'
+import useAuthContext from './use-auth-context'
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
   const { isAuthenticated } = useAuthContext()
 
   const { pathname } = useLocation()
-
   const [requestedLocation, setRequestedLocation] = useState<string | null>(null)
 
   if (!isAuthenticated) {

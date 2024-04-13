1097ceb6f9217e53399cceb0eccee48e7a82fae6
bally <bally.lomibao@howickltd.com>
2024-04-13 20:58:02 +1200

    moved bootcampApiSlice to handle bootcamp endpoints to its own folder

---
+++ src/store/slice/bootcamp/endpoint.ts
@@ -0,0 +1,20 @@
+import { apiSlice } from '../api'
+import { ServerPath } from 'route/path'
+
+export const bootcampApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllBootcamp: builder.query<any, void>({
+      query: () => ({
+        url: ServerPath.BOOTCAMP
+      }),
+      keepUnusedDataFor: 5
+    }),
+    getBootcamp: builder.query<any, string>({
+      query: (id) => ({
+        url: ServerPath.BOOTCAMP_ID(id)
+      })
+    })
+  })
+})
+
+export const { useGetAllBootcampQuery, useGetBootcampQuery } = bootcampApiSlice

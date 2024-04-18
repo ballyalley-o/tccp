619895bd10e70378da47249a11da9d28040ee108
bally <bally.lomibao@howickltd.com>
2024-04-19 07:44:35 +1200

    View | Course #2: Course component and add CourseSearch functionality

--- src/page/course/course.tsx
+++ src/page/course/course.tsx
@@ -1,18 +1,24 @@
 import { m } from 'framer-motion'
+import { useGetAllCourseQuery } from 'store/slice'
 import { MotionLazyContainer } from 'component/motion'
 import { Box, Grid } from '@mui/material'
+import { CourseCard } from 'section/course'
+import { SCard } from 'theme/style'
+import { SkeletonLoader } from 'component/skeleton'
 import { Typography } from 'component/typography'
 import { CourseSearch } from 'section/course'
 import { ASSET } from 'config'
 import { LABEL } from 'constant'
 const Course = () => {
+  const { data, error, isLoading } = useGetAllCourseQuery()
+
   return (
     <MotionLazyContainer>
       <Box
         sx={{
           margin: 10,
           px: 10,
-          height: '100vh'
+          height: '100%'
         }}>
         <Box mt={20}>
           <Box
@@ -30,12 +36,32 @@ const Course = () => {
             {LABEL.COURSE_PAGE_TITLE}
           </Typography>
         </Box>
-        <Typography variant='h1' color='initial'>
-          Course
-        </Typography>
         <Grid container flexDirection='row' spacing={2} sx={{ mt: 8, display: 'flex' }}>
           <Grid item sm={3}>
             <CourseSearch />
+          </Grid>
+
+          <Grid item sm={9}>
+            <SCard
+              sx={{
+                width: '100%',
+                display: 'flex',
+                bgcolor: 'transparent'
+              }}>
+              <Grid container flexDirection='row' flex={1} spacing={2} sx={{ backgroundColor: 'transparent' }}>
+                {isLoading ? (
+                  <Grid item lg={4}>
+                    <SkeletonLoader cards={8} />
+                  </Grid>
+                ) : (
+                  data?.data?.map((course: any, index: number) => (
+                    <Grid key={index} item lg={4}>
+                      <CourseCard key={index} course={course} />
+                    </Grid>
+                  ))
+                )}
+              </Grid>
+            </SCard>
           </Grid>
         </Grid>
       </Box>

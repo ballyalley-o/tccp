c97c4fafe6a9e4b44e4e6f8e3d247d6c153123fe
bally <bally.lomibao@howickltd.com>
2024-04-22 07:33:17 +1200

    Refactor CourseCard component and update bootcamp badge handling

--- src/section/course/course-card.tsx
+++ src/section/course/course-card.tsx
@@ -1,7 +1,8 @@
 import { FC, useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { dispatch } from 'store'
+import { useGetBootcampQuery } from 'store/slice'
 import { Box, CardContent, CardMedia, Chip, Grid, Typography, Rating } from '@mui/material'
-import { SScrollBox, SBadgeHeader } from 'theme/style'
 import { ServerPath } from 'route/path'
 import { KEY } from 'constant'
 import { ASSET } from 'config'
@@ -11,19 +12,19 @@ interface CourseCardProps {
   course: Course
 }
 
-function badgeLocation({ course }: CourseCardProps) {
-  return course?.bootcamp?.badge === KEY.BADGE_DEFAULT
-    ? ServerPath.ORIGIN + `/upload/badge/` + course?.bootcamp?.badge
-    : ServerPath.ORIGIN + `/upload/badge/${course?.bootcamp?._id}/` + course?.bootcamp?.badge
-}
-
 const CourseCard: FC<CourseCardProps> = ({ course }) => {
-  // const { bootcamp } = useSelector((state: any) => state.bootcamp)
+  const { data: bootcamp, error, isLoading } = useGetBootcampQuery(course?.bootcamp?._id)
 
   let minimumSkill = ''
 
   if (course.bootcamp) {
     minimumSkill = capitalize(course?.minimumSkill)
+  }
+
+  function badgeLocation(bootcamp: Bootcamp) {
+    return bootcamp?.badge === KEY.BADGE_DEFAULT
+      ? ServerPath.ORIGIN + `/upload/badge/` + bootcamp?.badge
+      : ServerPath.ORIGIN + `/upload/badge/${bootcamp?._id}/` + bootcamp?.badge
   }
 
   return (
@@ -43,28 +44,33 @@ const CourseCard: FC<CourseCardProps> = ({ course }) => {
               <Typography variant='h5' color='common.black'>
                 {course?.title.charAt(0).toUpperCase() + course?.title.slice(1)}
               </Typography>
+              <Grid container flex={1} flexDirection='row' gap={1}>
+                <Typography variant='subtitle2'>Duration: {course?.duration + ' weeks' || 'No duration specified'}</Typography>
+                <Typography variant='subtitle2'>Tuition: {course?.tuition || 'No tuition specified'}</Typography>
+              </Grid>
             </Grid>
             <Grid item xs={4}>
               <Grid container spacing={0} justifyContent='flex-end'>
                 <CardMedia
                   component='img'
-                  src={badgeLocation({ course })}
+                  src={course?.bootcamp === null ? ASSET.default_badge : badgeLocation(bootcamp?.data)}
                   alt='company badge'
                   height={50}
-                  sx={{ width: 50, display: 'flex', justifyContent: 'flex-end', borderRadius: 2 }}
+                  sx={{ width: 50, display: 'flex', borderRadius: 2 }}
                 />
               </Grid>
             </Grid>
           </Grid>
         </Box>
+
         <Typography variant='h6' py={2}>
-          {minimumSkill}
+          {minimumSkill || 'No minimum skill required'}
         </Typography>
-        <Box height={40}>
+        <Box height={130}>
           <Typography variant='subtitle2'>{course.description.charAt(0).toUpperCase() + course.description.slice(1)}</Typography>
         </Box>
         <Typography variant='h6' py={2}>
-          {course?.bootcamp?.name.charAt(0).toUpperCase() + course?.bootcamp?.name.slice(1)}
+          {course?.bootcamp?.name.charAt(0).toUpperCase() + course?.bootcamp?.name.slice(1) || 'Solo'}
         </Typography>
       </CardContent>
     </Grid>

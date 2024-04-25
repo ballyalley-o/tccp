fd6e65abe6be2f32b2f1b121f9d5abcf17fcda76
bally <bally.lomibao@howickltd.com>
2024-04-25 22:01:44 +1200

    TCCPISSUE: Refactor Bootcamp tile and Bootcamp #152 : Add category configuration file

---
+++ src/config/category.ts
@@ -0,0 +1,7 @@
+export const CATEGORY = {
+  GENERAL: 'General',
+  AI_DIGITAL_TRANSFORMATION: 'AI & Digital Transformation',
+  SUSTAINABILITY: 'Sustainability',
+  LEADERSHIP_INTERPERSONAL_SKILLS: 'Leadership & Interpersonal Skills',
+  DATABASES: 'Databases'
+}

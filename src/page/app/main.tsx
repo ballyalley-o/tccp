d6add5ebe08d7a6fe692893f5ca580256cf95c87
bally <bally.lomibao@howickltd.com>
2024-05-05 10:13:16 +1200

    [Feature] Dashboard Layout : #123: Add AppLayout component and related files

---
+++ src/page/app/main.tsx
@@ -0,0 +1,27 @@
+import { FC, ReactNode } from 'react'
+import { Box } from '@mui/material'
+import { HEADER, SPACING } from 'config/layout'
+
+interface MainProps {
+  children: ReactNode
+  isDesktop: boolean
+}
+
+const Main: FC<MainProps> = ({ children, isDesktop }) => {
+  return (
+    <Box
+      component='main'
+      sx={{
+        pt: `${HEADER.H_MOBILE + SPACING.SM}px`,
+        pb: `${HEADER.H_MOBILE + SPACING.SM}px`,
+        ...(isDesktop && {
+          px: 2,
+          pb: `${HEADER.H_DASHBOARD_DESKTOP + SPACING.SM}px`
+        })
+      }}>
+      {children}
+    </Box>
+  )
+}
+
+export default Main

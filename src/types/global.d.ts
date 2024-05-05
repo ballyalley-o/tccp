e6063019884ca5b8acdf4931a4fcf95868affc22
bally <bally.lomibao@howickltd.com>
2024-05-05 17:05:18 +1200

    Update Bootcamp interface properties in global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -90,36 +90,36 @@ declare global {
   }
 
   interface Bootcamp {
-    _id: Schema.Types.ObjectId
+    _id?: Schema.Types.ObjectId
     name: string
-    slug: string
-    description: string
-    website: string
-    phone: string
-    email: string
-    address: string
-    location: {
-      type: string
-      coordinates: [number | undefined, number | undefined]
-      formattedAddress: string
-      street: string
-      city: string
-      state: string
-      zipcode: string
-      country: string
+    slug?: string
+    description?: string
+    website?: string
+    phone?: string
+    email?: string
+    address?: string
+    location?: {
+      type?: string
+      coordinates?: [number | undefined, number | undefined]
+      formattedAddress?: string
+      street?: string
+      city?: string
+      state?: string
+      zipcode?: string
+      country?: string
     }
-    careers: [string]
-    duration: string
-    averageRating: number
-    averageCost: number
-    photo: string
-    badge: string
-    housing: boolean
-    jobAssistance: boolean
-    jobGuarantee: boolean
-    acceptGi: boolean
-    createdAt: Date
-    user: Schema.Types.ObjectId
+    careers?: [string]
+    duration?: string
+    averageRating?: number
+    averageCost?: number
+    photo?: string | undefined
+    badge?: string | undefined
+    housing?: boolean | undefined
+    jobAssistance?: boolean
+    jobGuarantee?: boolean
+    acceptGi?: boolean
+    createdAt?: Date
+    user?: Schema.Types.ObjectId
   }
 
   interface IResponse {
@@ -151,7 +151,7 @@ declare global {
     location?: Location
     website: string
     photo: string
-    rating: number
+    rating?: number
     careers?: string[] | null
   }
 
@@ -167,7 +167,7 @@ declare global {
   }
 
   interface UploadLocationProps {
-    bootcamp: BootcampCardProps
+    bootcamp?: Bootcamp | undefined
   }
 
   interface IDefaultAvatar {

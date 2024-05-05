804f38d9ffdaa4979869fe58b24a90a964fe0c6e
bally <bally.lomibao@howickltd.com>
2024-05-05 16:54:25 +1200

    Add new ARIA constant for course table in aria.ts

--- src/constant/aria.ts
+++ src/constant/aria.ts
@@ -5,7 +5,8 @@ enum ARIA {
   LAST_NAME = 'Last name',
   TOGGLE_PASSWORD = 'toggle password visibility',
   TABS = 'tabs',
-  COMPANY_BADGE = 'company badge'
+  COMPANY_BADGE = 'company badge',
+  COURSE_TABLE = 'course table'
 }
 
 export default ARIA

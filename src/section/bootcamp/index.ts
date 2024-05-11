f47ceb6ae657073f19616cecd8a04b6d37579783
bally <bally.lomibao@howickltd.com>
2024-05-11 21:06:27 +1200

    chore: Remove unused Bootcamp component and export statement

--- src/section/bootcamp/index.ts
+++ src/section/bootcamp/index.ts
@@ -1,4 +1,3 @@
-export { default as Bootcamp } from './bootcamp'
 export { default as BootcampSearch } from './bootcamp-search'
 export { default as BootcampCard } from './bootcamp-card'
 export { default as BootcampCareer } from './bootcamp-career'

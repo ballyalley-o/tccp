0dd86c65c0c82a0a08ecdda35ff22357d1156618
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-03-23 21:36:41 +1000

    [fix-linting]: change unused vars rule to warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     'react-refresh/only-export-components': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
+    "no-unused-vars": "warn",
     'react/jsx-no-target-blank': 'off',
     '@typescript-eslint/no-unused-vars-experimental': 'off',
     'react-refresh/only-export-components': [
